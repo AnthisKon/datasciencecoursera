a2c378cb752f3068517a79c5a5ce28d5e1913767
Konstantinos <kon.anthis@hotmail.com>
2018-01-02 09:39:50 +0200

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -2,14 +2,14 @@ library(dplyr)
 
 ## Read data and name variables
 #test
-x_test = read.table('C://Users//k.anthis//Desktop//Rmarkdown&Leaflet//ReproducibleRechearch//UCI HAR Dataset//test//X_test.txt')
-y_test = read.table('C://Users//k.anthis//Desktop//Rmarkdown&Leaflet//ReproducibleRechearch//UCI HAR Dataset//test//Y_test.txt')
-subject_test = data.frame(subject = readLines('C://Users//k.anthis//Desktop//Rmarkdown&Leaflet//ReproducibleRechearch//UCI HAR Dataset//test//subject_test.txt'))
+x_test = read.table('X_test.txt')
+y_test = read.table('Y_test.txt')
+subject_test = data.frame(subject = readLines('subject_test.txt'))
 
 #train
-x_train = read.table('C://Users//k.anthis//Desktop//Rmarkdown&Leaflet//ReproducibleRechearch//UCI HAR Dataset//train//X_train.txt')
-y_train = read.table('C://Users//k.anthis//Desktop//Rmarkdown&Leaflet//ReproducibleRechearch//UCI HAR Dataset//train//Y_train.txt')
-subject_train = data.frame(subject = readLines('C://Users//k.anthis//Desktop//Rmarkdown&Leaflet//ReproducibleRechearch//UCI HAR Dataset//train//subject_train.txt'))
+x_train = read.table('X_train.txt')
+y_train = read.table('Y_train.txt')
+subject_train = data.frame(subject = readLines('subject_train.txt'))
 
 
 # features
@@ -20,14 +20,14 @@ test = bind_cols(x_test,y_test,subject_test)
 train = bind_cols(x_train,y_train,subject_train)
 
 # Name variables with descriptive title
-names(test) = c(features,"Y","subject")
-names(train) = c(features,"Y","subject")
+names(test) = c(features,"Activities","subject")
+names(train) = c(features,"Activities","subject")
 
 # bind train and test set
 full_dataset = bind_rows(train,test)
 
 # name activities
-full_dataset$Y = factor(full_dataset$Y, 
+full_dataset$Activities = factor(full_dataset$Activities, 
                         levels = c(1:6), 
                         labels = c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING"))
 
@@ -41,8 +41,7 @@ only_set <- bind_cols(mean_set,std_set)
 
 # 5 second, independent tidy data set with the average of each variable for each activity and each subject.
 
-tidy_dataset <- full_dataset %>% group_by(Y,subject) %>% summarise_all(funs(mean))
+tidy_dataset <- full_dataset %>% group_by(Activities,subject) %>% summarise_all(funs(mean))
 
 ## write csv
-write.csv(tidy_dataset, 'tidy_dataset.txt')
-write.csv(full_dataset,'processed.txt')
+write.table(tidy_dataset, 'tidy_dataset.txt', row.name = F)
